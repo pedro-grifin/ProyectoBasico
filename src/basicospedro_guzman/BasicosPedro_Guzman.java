ab2863243d56fd31c10478728860feb8e1a3d783
Pedro Guzman <pedro.grifin@gmail.com>
2019-11-17 19:33:34 +0100

    Metodo numericos

--- src/basicospedro_guzman/BasicosPedro_Guzman.java
+++ src/basicospedro_guzman/BasicosPedro_Guzman.java
@@ -11,9 +11,29 @@ package basicospedro_guzman;
  */
 public class BasicosPedro_Guzman {
 
-    /**
-     * @param args the command line arguments
+      /**
+     * Método para mostrar ejemplos de tipos numericos.
      */
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
